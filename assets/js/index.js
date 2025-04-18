b01233b53d1f58d1f185f5a7b3244aff9c87fd6b
gaelraj <hei.gael.30@gmail.com>
2025-04-18 21:17:23 +0300

    Add word and letter highlighting functions for typing feedback

--- assets/js/index.js
+++ assets/js/index.js
@@ -53,6 +53,37 @@ const startTimer = () => {
     if (!startTime) startTime = Date.now();
 };
 
+const highlightCorrectLetters = () => {
+    const targetWord = wordsToType[currentWordIndex]; // Le mot à taper
+    const wordElements = wordDisplay.children;        // Tous les mots affichés
+    const currentWordSpan = wordElements[currentWordIndex]; // Le <span> du mot courant
+    let letterSpan = "";
+    let opacityStyle =  "opacity: 1;";
+   
+    for(let i = 0;i < targetWord.length;i++){
+        if(i < typedLetter.length){
+            if(typedLetter[i] === targetWord[i]){
+                letterSpan += `<span style="${opacityStyle}">${targetWord[i]}</span>`
+
+            }else{
+                letterSpan +=  `<span style="color: red;${opacityStyle}">${targetWord[i]}</span>`
+            }
+        }else {
+            letterSpan += `<span style="color: #AAAAAA;">${targetWord[i]}</span>`;
+        }
+    }
+    letterSpan += `<span> </span>`;
+    currentWordSpan.style.opacity = "1";
+    currentWordSpan.innerHTML = letterSpan;
+
+    currentWordSpan.scrollIntoView({
+        behavior: "smooth",
+        inline: "center",
+        block: "center"
+    });
+    
+};
+
 // Event listeners
 // Attach `updateWord` to `keydown` instead of `input`
 inputField.addEventListener("keydown", (event) => {
